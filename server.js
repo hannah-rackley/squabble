900c160e97ab9fb379e46577bb3421aa1f13f517
Hannah <hrglasser94@gmail.com>
2018-08-24 11:47:37 -0400

    Added in email and password authentication to the authenticate function.

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-let users = [{id: '1', name:'Hannah', email: 'hg@jangle.com'},
-{id: '2', name:'John', email: 'john@jingle.com'}]
+let users = [{id: '1', name:'Hannah', email: 'hg@jangle.com', password:'1234'},
+{id: '2', name:'John', email: 'john@jingle.com', password: '0000'}]
 
 let squabs = [{id: '1', body:'I have so many complaints', userId: '2'},
 {id: '2', body:'I have so few complaints!', userId: '1'}]
 
 const express = require('express');
+// const Router = express.Router;
 
 let app = express();
+// let router = new Router();
+// let protected = new Router();
 
-let authenticate = (req, res, callback) => {
-    if (req.query.password === 'letmein') {
-        callback();
-    } else {
-        res.end('You shall not pass');
-    }
+let authenticate = (req, res, next) => {
+    let password = req.query.password;
+    let email = req.query.email;
+    users.forEach(user => {
+        if (email === user.email && password === user.password) {
+            next();
+        }
+        // } else {
+        //     res.end('You shall not pass');
+        // }
+    })
+    res.end('You shall not pass');
 }
 
 let listUsers = (req, res) => {
@@ -21,13 +30,15 @@ let listUsers = (req, res) => {
 }
 
 let squabsByUser = (req, res) => {
-    let userId = req.params.userId;
+    let userId = req.params.userId
     let mySquabs = squabs.filter(squab => 
     squab.userId === userId);
     res.send(mySquabs)
 }
 
-app.get('/users', authenticate, listUsers)
-app.get('/users/:userId/squabs', authenticate, squabsByUser)
+app.get('/users', authenticate, listUsers);
+app.get('/users/:userId/squabs', authenticate, squabsByUser);
+// router.use(authenticate, protected);
+// app.use(router);
 
-app.listen(3002)
+app.listen(3002);
